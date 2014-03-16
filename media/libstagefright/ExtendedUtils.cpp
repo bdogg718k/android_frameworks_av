b9b11da575408e2231b1474553383782422d7916
westcripp <korukaltan@gmail.com>
2014-03-16 04:07:10 +0200

    Revert "stagefright: Disable HSR for now"
    This reverts commit 4eef3ad6037b6f1db2a72da1c46728f70a15bfb0.

--- media/libstagefright/ExtendedUtils.cpp
+++ media/libstagefright/ExtendedUtils.cpp
@@ -74,7 +74,7 @@ void ExtendedUtils::HFR::setHFRIfEnabled(
         hfr = 0;
     }
 
-#if 0
+#ifndef LEGACY_MEDIA
     const char *hsr_str = params.get("video-hsr");
 
     if(hsr_str && !strncmp(hsr_str,"on",2)) {
@@ -90,7 +90,7 @@ status_t ExtendedUtils::HFR::initializeHFR(
         int64_t &maxFileDurationUs, video_encoder videoEncoder) {
     status_t retVal = OK;
 
-#if 0
+#ifndef LEGACY_MEDIA
     //Check HSR first, if HSR is enable set HSR to kKeyFrameRate
     int32_t hsr =0;
     if (meta->findInt32(kKeyHSR, &hsr)) {
